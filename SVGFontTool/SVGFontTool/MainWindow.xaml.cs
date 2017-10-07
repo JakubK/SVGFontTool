aef8faca40cc970726e9c199f2d5116100d1be38
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-10-07 16:50:55 +0200

    Acquire Glyph names and PathData from SVG Font

--- SVGFontTool/SVGFontTool/MainWindow.xaml.cs
+++ SVGFontTool/SVGFontTool/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,7 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Xml;
+using System.Xml.Linq;
 using FolderBrowserDialog = System.Windows.Forms.FolderBrowserDialog;
 
 namespace SVGFontTool
@@ -31,6 +35,7 @@ namespace SVGFontTool
         private void Browse_Font(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fontDialog = new OpenFileDialog();
+            fontDialog.Filter = "SVG File (*.svg) | *.svg";
             if (fontDialog.ShowDialog() == true)
                 txtFont.Text = fontDialog.InitialDirectory + fontDialog.FileName;
         }
@@ -44,9 +49,25 @@ namespace SVGFontTool
 
         private void Confirm(object sender, RoutedEventArgs e)
         {
-            if(txtFont.Text != null && txtDestination.Text != null && txtEnum.Text != null)
+            if(!string.IsNullOrEmpty(txtFont.Text)&& !string.IsNullOrEmpty(txtDestination.Text) && !string.IsNullOrEmpty(txtEnum.Text))
             {
-                //Perform Reading and Writing logic
+               
+                if (File.Exists(txtFont.Text) && Directory.Exists(txtDestination.Text))
+                {
+                    List<string> GlyphNames = new List<string>();
+                    List<string> PathDatas = new List<string>();
+                    XmlTextReader reader = new XmlTextReader(txtFont.Text);
+                    XmlNodeType type;
+                    while(reader.Read())
+                    {
+                        type = reader.NodeType;
+                        if (reader.Name == "glyph")
+                        {
+                            GlyphNames.Add(reader.GetAttribute("glyph-name"));
+                            PathDatas.Add(reader.GetAttribute("d"));
+                        }
+                    }
+                }
             }
         }
     }
